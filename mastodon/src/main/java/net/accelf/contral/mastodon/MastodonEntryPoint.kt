2e180b65f9cbd803588a66b5b02a351dfbb7ec3b
kyori19 <kyori@accelf.net>
2022-08-21 05:55:18 +0900

    Add Mastodon plugin

---
+++ mastodon/src/main/java/net/accelf/contral/mastodon/MastodonEntryPoint.kt
@@ -0,0 +1,13 @@
+package net.accelf.contral.mastodon
+
+import net.accelf.contral.api.plugin.MinorVersion.Companion.minor
+import net.accelf.contral.api.plugin.MinorVersion.Companion.patch
+import net.accelf.contral.api.plugin.PluginResolver
+
+@Suppress("unused")
+fun PluginResolver.mastodonPlugin() {
+    name = "Mastodon"
+    version = 0 minor 0 patch 0
+
+    require("core", 0 minor 0)
+}

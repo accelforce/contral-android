1a5379bb31f93be2a47d685ba04bc46508f44045
kyori19 <kyori@accelf.net>
2022-08-29 02:02:54 +0900

    [mastodon] Add boost icon

--- mastodon/src/main/java/net/accelf/contral/mastodon/api/Status.kt
+++ mastodon/src/main/java/net/accelf/contral/mastodon/api/Status.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cached
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -40,6 +44,13 @@ data class Status(
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
                 ) {
+                    Icon(
+                        imageVector = Icons.Default.Cached,
+                        contentDescription = "Boost",
+                        modifier = Modifier.graphicsLayer(rotationY = 180f),
+                        tint = MaterialTheme.colorScheme.primary,
+                    )
+
                     AsyncImage(
                         model = account.avatar,
                         contentDescription = account.acct,

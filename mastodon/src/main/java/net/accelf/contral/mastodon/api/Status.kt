648a02cc84e90bf9b171105fc326425ff29902bb
kyori19 <kyori@accelf.net>
2022-09-01 10:28:48 +0900

    [mastodon] Respect contents warning

--- mastodon/src/main/java/net/accelf/contral/mastodon/api/Status.kt
+++ mastodon/src/main/java/net/accelf/contral/mastodon/api/Status.kt
@@ -1,18 +1,25 @@
 package net.accelf.contral.mastodon.api
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Cached
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,6 +33,7 @@ import net.accelf.contral.api.timelines.LocalTimeline
 import net.accelf.contral.api.timelines.TimelineItem
 import net.accelf.contral.api.ui.LocalNavController
 import net.accelf.contral.api.ui.theme.ContralTheme
+import net.accelf.contral.api.ui.utils.useState
 import net.accelf.contral.mastodon.timelines.HomeTimeline
 import net.accelf.contral.mastodon.ui.Html
 import net.accelf.contral.mastodon.ui.HtmlAnnotations
@@ -37,17 +45,26 @@ data class Status(
     @SerialName("account") val account: Account,
     @SerialName("content") val content: Html,
     @SerialName("reblog") val boostedStatus: Status?,
+    @SerialName("spoiler_text") val contentsWarning: String,
 ) : TimelineItem {
 
     @Composable
     override fun Render() {
-        val uriHandler = LocalUriHandler.current
         val navController = LocalNavController.current
         val timeline = LocalTimeline.current as HomeTimeline
 
-        val openAccount = {
-            navController.navigate(account.path(timeline.dbAccount))
-        }
+        Render(
+            openAccount = {
+                navController.navigate(account.path(timeline.dbAccount))
+            },
+        )
+    }
+
+    @Composable
+    internal fun Render(
+        openAccount: (Account) -> Unit,
+    ) {
+        val uriHandler = LocalUriHandler.current
 
         if (boostedStatus != null) {
             Column {
@@ -67,12 +84,12 @@ data class Status(
                         modifier = Modifier
                             .size(24.dp)
                             .padding(2.dp)
-                            .clickable { openAccount() },
+                            .clickable { openAccount(account) },
                     )
 
                     Text(
                         text = account.displayName,
-                        modifier = Modifier.clickable { openAccount() },
+                        modifier = Modifier.clickable { openAccount(account) },
                         maxLines = 1,
                         style = MaterialTheme.typography.titleSmall,
                     )
@@ -81,13 +98,15 @@ data class Status(
                         text = "@${account.acct}",
                         modifier = Modifier
                             .padding(start = 4.dp)
-                            .clickable { openAccount() },
+                            .clickable { openAccount(account) },
                         maxLines = 1,
                         style = MaterialTheme.typography.labelSmall,
                     )
                 }
 
-                boostedStatus.Render()
+                boostedStatus.Render(
+                    openAccount = openAccount,
+                )
             }
         } else {
             Row {
@@ -97,13 +116,13 @@ data class Status(
                     modifier = Modifier
                         .size(48.dp)
                         .padding(2.dp)
-                        .clickable { openAccount() },
+                        .clickable { openAccount(account) },
                 )
 
                 Column {
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.clickable { openAccount() },
+                        modifier = Modifier.clickable { openAccount(account) },
                     ) {
                         Text(
                             text = account.displayName,
@@ -119,14 +138,40 @@ data class Status(
                         )
                     }
 
-                    HtmlText(
-                        html = content,
-                    ) { annotations ->
-                        annotations.forEach { annotation ->
-                            when (annotation.tag) {
-                                HtmlAnnotations.URL.tag -> {
-                                    uriHandler.openUri(annotation.item)
-                                    return@HtmlText
+                    var hideContents by useState(contentsWarning.isNotBlank())
+
+                    if (contentsWarning.isNotBlank()) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable { hideContents = !hideContents },
+                        ) {
+                            val iconRotation by animateFloatAsState(if (hideContents) -90f else 0f)
+                            Icon(
+                                imageVector = Icons.Default.ExpandMore,
+                                contentDescription = "Expand",
+                                modifier = Modifier.rotate(iconRotation),
+                            )
+
+                            Text(
+                                text = contentsWarning,
+                                maxLines = 1,
+                            )
+                        }
+                    }
+
+                    AnimatedVisibility(
+                        visible = !hideContents,
+                    ) {
+                        HtmlText(
+                            html = content,
+                        ) { annotations ->
+                            annotations.forEach { annotation ->
+                                when (annotation.tag) {
+                                    HtmlAnnotations.URL.tag -> {
+                                        uriHandler.openUri(annotation.item)
+                                        return@HtmlText
+                                    }
                                 }
                             }
                         }
@@ -147,6 +192,7 @@ internal class PreviewStatusProvider : PreviewParameterProvider<Status> {
                 account = accountProvider.values.elementAt(0),
                 content = "Hi!<br>This is a sample post.",
                 boostedStatus = null,
+                contentsWarning = "CW",
             ),
             Status(
                 id = "234567890234567890",
@@ -157,7 +203,9 @@ internal class PreviewStatusProvider : PreviewParameterProvider<Status> {
                     account = accountProvider.values.elementAt(0),
                     content = "Hi!<br>This is a sample post.",
                     boostedStatus = null,
+                    contentsWarning = "",
                 ),
+                contentsWarning = "",
             ),
         )
 }
@@ -168,6 +216,8 @@ private fun PreviewStatus(
     @PreviewParameter(PreviewStatusProvider::class) status: Status,
 ) {
     ContralTheme {
-        status.Render()
+        status.Render(
+            openAccount = {},
+        )
     }
 }
